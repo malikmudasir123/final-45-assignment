9960f183bff454d763ba7babd0527de9395f6428
MALIK MUDASIR <160151107+malikmudasir123@users.noreply.github.com>
2024-02-29 16:10:38 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Nothing is impodssible because impossible itself I M POSSIBLE";
+let author = "Malik Mudasir";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,10 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impossible because impossible itself I M POSSIBLE"
+
+let author:string = "Malik Mudasir";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
+
